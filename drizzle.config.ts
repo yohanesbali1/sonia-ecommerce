df2fe1df28a570c1519b20543f7d0d2df2256d20
yohanes <yohanesaditia1312@gmail.com>
2026-08-28 02:03:16 +0800

    hotfix config drizzel.config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: ".env.local",
+});
+
 
 export default defineConfig({
   schema: './src/lib/db/tables.ts',
